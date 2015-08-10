9f88c05f7e1c7638de664b7ead6a5d4373e74c94
meta-babalu-patidar <babalu.patidar@metacube.com>
2015-08-10 12:43:14 +0530

    Update JoinTwoSortedArray.java

--- PF/Assignment1/JoinTwoSortedArray.java
+++ PF/Assignment1/JoinTwoSortedArray.java
@@ -1,9 +1,14 @@
+/*************************
+	 * @author Babalu Patidar
+	 * Program to Join two sorted Array
+	 * Join method takes two sorted array and size as input and return one joined sorted arrray
+	 ***********/
 import java.util.Arrays;
 
-
+//starting of class
 public class JoinTwoSortedArray {
 
-public  int[] join(int a[], int asize, int b[], int bsize, int c[])
+public  int[] join(int a[], int asize, int b[], int bsize, int c[])//Join method takes two sorted array and size as input and return one joined sorted arrray
 	
 	{
 	
@@ -39,6 +44,6 @@ public  int[] join(int a[], int asize, int b[], int bsize, int c[])
 		
 		
 		
-	}
+	}//End of join method
 	
-}
+}//End of class
