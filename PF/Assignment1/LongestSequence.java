9af6fb0a3c9ce600ccdb6a337f45e0b7c214b7cb
meta-babalu-patidar <babalu.patidar@metacube.com>
2015-08-10 12:47:41 +0530

    Update LongestSequence.java

--- PF/Assignment1/LongestSequence.java
+++ PF/Assignment1/LongestSequence.java
@@ -1,7 +1,12 @@
-
+/*************************
+	 * @author Babalu Patidar
+	 * Program to Find longestSequnce 
+	 * longest method take an array and return longest sequnce string
+	 ***********/
 
 import java.util.Arrays;
  
+ //starting of class
 public class LongestSequence 
 {
 
@@ -9,7 +14,7 @@ public class LongestSequence
 	int k=0;
 
 	int count = 1;  int max = 1;int start=0;int last=0;
-String longest(int a[])
+String longest(int a[]) //longest method take an array and return longest sequnce string
 {
 	
 	for (int i = 1; i < a.length; i++) 
@@ -38,8 +43,8 @@ String longest(int a[])
 	}
 	System.out.println(Arrays.toString(b));
 	return Arrays.toString(b);
- }
+ }//End of method
 
-}
+}//End of class 
 
  
