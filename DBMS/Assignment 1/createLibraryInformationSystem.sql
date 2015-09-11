b13175af45e85e46fd2c94ae88231567363781bf
meta-babalu-patidar <babalu.patidar@metacube.com>
2015-09-11 10:36:14 +0530

    Update createLibraryInformationSystem.sql

--- DBMS/Assignment 1/createLibraryInformationSystem.sql
+++ DBMS/Assignment 1/createLibraryInformationSystem.sql
@@ -1,7 +1,10 @@
+-- 2015-09-08: Babalu patidar : created.
+
+
 CREATE DATABASE Library;
 
 USE Library;
-
+-- Creating Members table
 CREATE TABLE Members(
 member_id INT auto_increment PRIMARY KEY,
 member_nm VARCHAR(100),
@@ -10,21 +13,25 @@ addressline2 VARCHAR(100),
 category char(1)
 );
 
+-- Creating subjects table 
 CREATE TABLE Subjects(
 subject_id INT auto_increment PRIMARY KEY,
 subject_nm VARCHAR(100)
 );
 
+-- Creating publishres table
 CREATE TABLE Publishers(
 publisher_id INT PRIMARY KEY,
 publisher_nm VARCHAR(100)
 );
 
+-- Creating authors table
 CREATE TABLE Authors(
 author_id INT auto_increment PRIMARY KEY,
 author_nm VARCHAR(100)
 );
 
+-- Creating titles table
 CREATE TABLE Titles(
 title_id INT auto_increment PRIMARY KEY,
 title_nm VARCHAR(100),
@@ -34,6 +41,7 @@ FOREIGN KEY (subject_id) REFERENCES Subjects(subject_id) ON DELETE CASCADE,
 FOREIGN KEY (publisher_id) REFERENCES Publishers(publisher_id) ON DELETE CASCADE
 );
 
+-- Creating title_author table 
 CREATE TABLE Title_author(
 title_id INT,
 author_id INT,
@@ -42,6 +50,8 @@ FOREIGN KEY (title_id) REFERENCES Titles(title_id) ON DELETE CASCADE,
 FOREIGN KEY (author_id) REFERENCES Authors(author_id) ON DELETE CASCADE
 );
 
+
+-- Creating books table
 CREATE TABLE Books(
 accession_no INT auto_increment PRIMARY KEY,
 title_id INT,
@@ -52,6 +62,8 @@ CHECK (status = 0 OR status = 1),
 FOREIGN KEY (title_id) REFERENCES Titles(title_id) ON DELETE CASCADE
 );
 
+
+-- Creating book_issue table
 CREATE TABLE Book_issue(
 issue_dt DATE,
 accession_no INT,
@@ -62,6 +74,8 @@ FOREIGN KEY (accession_no) REFERENCES Books(accession_no) ON DELETE CASCADE,
 FOREIGN KEY (member_id) REFERENCES Members(member_id) ON DELETE CASCADE
 );
 
+
+-- Creating book_return table
 CREATE TABLE Book_return(
 return_dt DATE,
 accession_no INT,
@@ -74,6 +88,8 @@ FOREIGN KEY (member_id) REFERENCES Members(member_id) ON DELETE CASCADE
 
 SHOW TABLES;
 
+-- Alter definitions of following LIS tables to provide the default constraints
+
 ALTER TABLE `Book_issue` MODIFY COLUMN `issue_dt` TIMESTAMP NOT NULL DEFAULT NOW();
 DELIMITER ;;
 CREATE TRIGGER book_due_date BEFORE INSERT ON Book_issue FOR EACH ROW
@@ -82,6 +98,7 @@ BEGIN
 END;;
 DELIMITER ;
 
+-- Remove Members table of the LIS database.
 DROP TRIGGER book_due_date;
 DROP TABLE Book_issue;
 DROP TABLE Book_return;
@@ -89,6 +106,7 @@ DROP TABLE Members;
 
 SHOW TABLES;
 
+-- Creating the members table again
 CREATE TABLE Members(
 member_id INT auto_increment PRIMARY KEY,
 member_nm VARCHAR(100),
