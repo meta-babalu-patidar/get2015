68c674c5fdb4d549a35cb5f80739273d929a40f7
meta-babalu-patidar <babalu.patidar@metacube.com>
2015-09-11 10:57:20 +0530

    Update selectOperations2.sql

--- DBMS/DBMS SESSION 2/selectOperations2.sql
+++ DBMS/DBMS SESSION 2/selectOperations2.sql
@@ -1,5 +1,5 @@
 -- Assignment 2
-
+-- Modify Select command of question 9 to display results in ascending order of issue_dt and ascending order of members's name 
 SELECT bi.issue_dt, bi.accession_no, bi.member_id, m.member_nm,
    IF (br.return_dt is NULL,"",return_dt) as return_date
 FROM Book_issue bi
