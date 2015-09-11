46acd3ac189a2b52fbf5bfe764e83034a83387c9
meta-babalu-patidar <babalu.patidar@metacube.com>
2015-09-11 10:42:17 +0530

    Update runAll.sql

--- DBMS/Assignment 1/runAll.sql
+++ DBMS/Assignment 1/runAll.sql
@@ -1,3 +1,9 @@
+-- 2015-09-08: Babalu patidar : created.
+-- run command for createLibraryInformationSystem.sql script
 source C:/users/babalu/documents/createLibraryInformationSystem.sql;
+
+-- run command for insertAllTables.sql script
 source C:/users/babalu/documents/insertAllTables.sql;
+
+-- run command for libraryInformationSystemOperation.sql script
 source C:/users/babalu/documents/libraryInformationSystemOperation.sql;
