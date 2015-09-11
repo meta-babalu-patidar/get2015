e2dd7faf338b0c3006ba08179ea7b3f222b6b4bd
meta-babalu-patidar <babalu.patidar@metacube.com>
2015-09-11 11:02:57 +0530

    Update selectOperations2.sql

--- DBMS/DBMS SESSION 2/selectOperations2.sql
+++ DBMS/DBMS SESSION 2/selectOperations2.sql
@@ -1,3 +1,5 @@
+-- 2015-09-10: Babalu patidar : created.
+
 -- Assignment 2
 -- Modify Select command of question 9 to display results in ascending order of issue_dt and ascending order of members's name 
 SELECT bi.issue_dt, bi.accession_no, bi.member_id, m.member_nm,
