6dae5bdbe706e64dc7e37a94ee8295188a33b342
Babalu Patidar <babalu.patidar@metacube.com>
2015-10-28 18:01:30 +0530

    Jquery Assignment 1

---
+++ Web/JQuery/bin/Assignmnet-1/JavaScript/ToggleWithoutJquery.js
@@ -0,0 +1,33 @@
+function showORHide1(){
+		document.getElementById("span").style.visibility = "hidden";
+		document.getElementById("button3").value = "Show";
+		document.getElementById("button1").disabled = true;
+		document.getElementById("button2").disabled = false;
+	
+}
+
+function showORHide2(){
+	document.getElementById("span").style.visibility = "visible";
+	document.getElementById("button3").value = "Hide";
+	document.getElementById("button2").disabled = true;
+	document.getElementById("button1").disabled = false;
+
+}
+
+function showORHide3(){
+	if(document.getElementById("button3").value == "Hide"){
+		document.getElementById("span").style.visibility = "hidden";
+		document.getElementById("button3").value = "Show";
+		document.getElementById("button1").disabled = true;
+		document.getElementById("button2").disabled = false;
+	}
+	else{
+		document.getElementById("button3").value = "Hide";
+		document.getElementById("span").style.visibility = "visible";
+		document.getElementById("button2").disabled = true;
+		document.getElementById("button1").disabled = false;
+	}
+	
+}
+
+
