98a02e1925d65ed09379f21f708ac2245f15e311
Babalu <babalu.patidar@metacube.com>
2015-12-07 23:59:00 +0530

    project submission

--- DBMS/Assignment 1/runAll.sql
+++
@@ -1,9 +0,0 @@
--- 2015-09-08: Babalu patidar : created..
--- run command for createLibraryInformationSystem.sql script
-source C:/users/babalu/documents/createLibraryInformationSystem.sql;
-
--- run command for insertAllTables.sql script
-source C:/users/babalu/documents/insertAllTables.sql;
-
--- run command for libraryInformationSystemOperation.sql script
-source C:/users/babalu/documents/libraryInformationSystemOperation.sql;
